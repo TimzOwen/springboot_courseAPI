074a3271c3125a3d8937e6aea88a5af31790e24e
TimzOwen <timzowen@gmail.com>
2022-06-07 04:02:43 +0300

    updated request

--- src/main/java/coursesapi/topics/TopicService.java
+++ src/main/java/coursesapi/topics/TopicService.java
@@ -6,17 +6,19 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
-public class TopicService {
-	
+public class TopicService {	
 	List<Topics> topics = Arrays.asList(
 			new Topics("spring","Intro to spring","Welcome to spring"),
 			new Topics("Springboot","Intro to springboot","self explanatory"),
-			new Topics("spring","Intro to spring","Welcome to spring"),
-			new Topics("Springboot","Intro to springboot","self explanatory")
-			);
+			new Topics("English","Intro to spring","Welcome to spring"),
+			new Topics("Luo","Intro to springboot","self explanatory")
+			);	
 	
 	public List<Topics> getAllTopics() {
 		return topics;
 	}
-
+	
+	public Topics getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
 }
--- src/main/java/coursesapi/topics/TopicsController.java
+++ src/main/java/coursesapi/topics/TopicsController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +18,10 @@ public class TopicsController {
 	@RequestMapping("topics")
 	public List<Topics> getAllTopics() {
 		return topicservice.getAllTopics();
+	}
+	
+	@RequestMapping("/topics/{id}")
+	public Topics getTopic(@PathVariable String id) {
+		return topicservice.getTopic(id);
 	}
 }
